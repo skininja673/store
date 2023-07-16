c8cb09d23f402fbdb11a5dfa660b7a552b2f12ff
AL <skininja673@gmail.com>
2023-07-16 09:29:56 -0400

    made sidebar functional

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,14 +9,24 @@ import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
-    const isOpen = false;
+    const isOpen = true;
+    const { isSidebarOpen, closeSidebar } = useProductsContext();
+    console.log(isSidebarOpen);
 
     return (
         <SidebarContainer>
-            <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+            <aside
+                className={`${
+                    isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'
+                }`}
+            >
                 <div className='sidebar-header'>
                     <img src={logo} alt='company logo' className='logo' />
-                    <button className='close-btn' type='button'>
+                    <button
+                        className='close-btn'
+                        type='button'
+                        onClick={closeSidebar}
+                    >
                         <FaTimes />
                     </button>
                 </div>
@@ -25,7 +35,9 @@ const Sidebar = () => {
                     {links.map(({ id, text, url }) => {
                         return (
                             <li key={id}>
-                                <Link to={url}>{text}</Link>
+                                <Link to={url} onClick={closeSidebar}>
+                                    {text}
+                                </Link>
                             </li>
                         );
                     })}
