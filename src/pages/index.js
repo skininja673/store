8b5dea270bb271dbbd1ac150a630bff7c219e905
AL <skininja673@gmail.com>
2023-07-15 23:39:50 -0400

    added react router

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import Private from './PrivateRoute';
+
+export { About, Home, Products, SingleProduct, Cart, Error, Checkout, Private };
