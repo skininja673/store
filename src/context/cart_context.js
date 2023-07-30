43a937fc786abe3743f8825e58b6ddf7f642bdeb
AL <skininja673@gmail.com>
2023-07-30 17:28:11 -0400

    added hero page in add to cart component

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,23 +1,51 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/cart_reducer'
+import React, { useEffect, useContext, useReducer } from 'react';
+import reducer from '../reducers/cart_reducer';
 import {
-  ADD_TO_CART,
-  REMOVE_CART_ITEM,
-  TOGGLE_CART_ITEM_AMOUNT,
-  CLEAR_CART,
-  COUNT_CART_TOTALS,
-} from '../actions'
+    ADD_TO_CART,
+    REMOVE_CART_ITEM,
+    TOGGLE_CART_ITEM_AMOUNT,
+    CLEAR_CART,
+    COUNT_CART_TOTALS,
+} from '../actions';
 
-const initialState = {}
+const initialState = {
+    cart: [],
+    total_items: 0,
+    total_mount: 0,
+    shipping_fee: 534, //shipping fee is 5$ 34cents
+};
 
-const CartContext = React.createContext()
+const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
-  return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
-  )
-}
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // add to cart
+    const addToCart = (id, color, amount, product) => {
+        dispatch({
+            type: ADD_TO_CART,
+            payload: { id, color, amount, product },
+        });
+        return;
+    };
+
+    //remove item
+    const removeItem = (id) => {};
+
+    // toggle amount
+    const toggleAmount = (id, value) => {};
+    // clear cart
+    const clearCart = () => {};
+
+    return (
+        <CartContext.Provider
+            value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+};
 // make sure use
 export const useCartContext = () => {
-  return useContext(CartContext)
-}
+    return useContext(CartContext);
+};
